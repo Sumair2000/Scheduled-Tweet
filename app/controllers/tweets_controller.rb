0aad89678ed5944990f925abfcb6013fa1c68448
Sumair Ahmed <sumair.ahmed8p2@gmail.com>
2021-02-13 22:01:09 +0500

    Final

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,46 @@
+class TweetsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :set_tweet, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @tweets = Current.user.tweets
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was updated successfuly!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was  unscheduled!"
+  end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "Successfuly scheduled a tweet!"
+    else
+      render :new    
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@
 # password: string (virtual)
 # password_confirmation (virtual)
 class User < ApplicationRecord
+
+  has_many :twitter_accounts
   has_secure_password
+  has_many :tweets
+
 end
